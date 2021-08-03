e5af844d3d9d0c1acfb8ea884dbee0f084e328ca
nabby27 <ivancordoba77@gmail.com>
2021-08-03 22:25:52 +0200

    refactor

--- src/app/Game.ts
+++ src/app/Game.ts
@@ -1,12 +1,20 @@
 export class Game {
 
-    private _score = 0;
+    private _rolls: number[] = new Array(21).fill(0);
+    private _currentRoll = 0;
 
     public roll(pinsKnockedDown: number): void {
-        this._score += pinsKnockedDown;
+        this._rolls[this._currentRoll] = pinsKnockedDown;
+        this._currentRoll++;
     }
 
     public score(): number {
-        return this._score;
+        let score = 0;
+
+        for (let i = 0; i < this._rolls.length; i++) {
+            score += this._rolls[i];
+        }
+
+        return score;
     }
 }
